7cbb6475d1d7a9740cb2fa8b0fdc3b72aa236b4b
Dmytro Zubelevych <dz@jelastic.com>
2019-01-14 17:33:37 +0200

    JE-43957 [Jahia] Make incomplete backups to be invisible for end-users

--- scripts/getBackups.sh
+++ scripts/getBackups.sh
@@ -4,13 +4,23 @@ USER=$1
 
 ENV_LIST=$(ls -Qm /backups/${USER})
 
+BACKUP_EXCLUDE_LIST=""
+
 OUTPUT_JSON="{\"result\": 0, \"envs\": [${ENV_LIST}], \"backups\": {"
 
 if [ -n "$ENV_LIST" ]; then
 
     for i in $(ls /backups/${USER})
     do
-        DIRECTORY_LIST=$(ls -Qm /backups/${USER}/${i})
+        FULL_LIST=$(ls /backups/${USER}/${i})
+        for backup_dir in ${FULL_LIST}
+        do
+            FILE_NUMBER=$(find /backups/${USER}/${i}/${backup_dir} -maxdepth 1 -type f|wc -l)
+            if [ "${FILE_NUMBER}" -lt 3 ]; then
+                BACKUP_EXCLUDE_LIST="${BACKUP_EXCLUDE_LIST} --ignore=${backup_dir}"
+            fi 
+        done
+        DIRECTORY_LIST=$(ls -Qm /backups/${USER}/${i} ${BACKUP_EXCLUDE_LIST})
         OUTPUT_JSON="${OUTPUT_JSON}\"${i}\":[${DIRECTORY_LIST}],"
     done
 
