8d20d553bef335e5756c441180019babd89083fb
Dmytro Zubelevych <dz@jelastic.com>
2019-01-15 16:21:31 +0200

    hardcode removed

--- scripts/getJahiaVersion.js
+++ scripts/getJahiaVersion.js
@@ -6,7 +6,7 @@ var storage = use("scripts/useStorageApi.js");
 
 var userData = storage.getUserData();
 var ftpUser = userData.credentials.ftpUser;
-var version = storage.getVersion(ftpUser, ${settings.envName}, ${settings.backupDir});
+var version = storage.getVersion(ftpUser, "${settings.envName}", "${settings.backupDir}");
 if (version.result != 0) {
     version = { result: 0, version: "7.2.1.1" } //for compatibility with old backups where Jahia version is not saved
 }
