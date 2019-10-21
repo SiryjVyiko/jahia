42f1a8ef0ef19d84e0c5b7d7b2326944dda27c7a
SiryjVyiko <dz@hivext.net>
2019-10-21 12:26:19 +0300

    Update backup-manager.js

--- scripts/backup-manager.js
+++ scripts/backup-manager.js
@@ -119,11 +119,7 @@ function BackupManager(config) {
             }],
             [ me.cmd, [
                 'yum -y install lftp',
-                lftp.cmd([
-                    "cd %(envName)/%(backupDir)",
-                    "pget %(envName)/%(backupDir)/db_credentials"
-                ]),
-                'source db_credentials',
+                'source $(' + lftp.cmd("cat %(envName)/%(backupDir)/db_credentials") + ')',
                 'mysqldump --user=${DB_USER} --password=${DB_PASSWORD} --single-transaction --quote-names --opt --databases --compress jahia > jahia.sql',
                 'wget -q %(excludeListUrl) -O variables_exclude_list',
                 'grep -v -f variables_exclude_list /.jelenv > variables_sqldb',
