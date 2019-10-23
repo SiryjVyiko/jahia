af084f610040b164f517762ba1a4c6bae8bc6d9c
SiryjVyiko <dz@hivext.net>
2019-10-23 17:54:55 +0300

    Update setupUser.sh

--- scripts/setupUser.sh
+++ scripts/setupUser.sh
@@ -1,29 +1,16 @@
 #!/bin/bash
 
-user=$1
-pswd=$2
-
+USER=$1
+PASSWORD=$2
+ADMIN_PASSWORD=`pwgen 10 1`
+JEM=`which jem`
+MYSQL=`which mysql`
+cmd="CREATE USER '$USER'@'localhost' IDENTIFIED BY '$PASSWORD'; CREATE USER '$USER'@'%' IDENTIFIED BY '$PASSWORD'; GRANT ALL PRIVILEGES ON *.* TO '$USER'@'localhost' WITH GRANT OPTION; GRANT ALL PRIVILEGES ON *.* TO '$USER'@'%' WITH GRANT OPTION; FLUSH PRIVILEGES;"
 unset resp;
-resp=$(mysql -u$user -p$pswd mysql --execute="SHOW COLUMNS FROM user")
+resp=$(mysql -u$USER -p$PASSWORD mysql --execute="SHOW COLUMNS FROM user")
 [ -z "$resp" ] && {
-        service mysql stop;
-        mysqld_safe --skip-grant-tables --user=mysql --pid-file=/var/lib/mysql/$(hostname).pid &
-        sleep 5
-        cmd="CREATE TEMPORARY TABLE tmptable SELECT * FROM user WHERE User = 'root'; UPDATE tmptable SET User = '$user' WHERE User = 'root'; DELETE FROM user WHERE User = '$user'; INSERT INTO user SELECT * FROM tmptable WHERE User = '$user'; DROP TABLE tmptable;"
-        mysql mysql --execute="$cmd"
-
-        version=$(mysql --version|awk '{ print $5 }'|awk -F\, '{ print $1 }')
-
-        if (( $(awk 'BEGIN {print ("'$version'" >= "'5.7'")}') )); then
-                cmd="UPDATE user SET authentication_string=PASSWORD('$pswd') WHERE user='$user';";
-        else
-                cmd="UPDATE user SET password=PASSWORD('$pswd') WHERE user='$user';";
-        fi
-
-        mysql mysql --execute="$cmd"
-        echo $resp
-        rm -f /var/lib/mysql/auto.cnf
-        service mysql restart
-} || { 
-        echo "[Info] User $user has the required access to the database." 
+   $JEM passwd set -p $ADMIN_PASSWORD
+   $MYSQL -uroot -p${ADMIN_PASSWORD} --execute="$cmd"
+} || {
+   echo "[Info] User $user has the required access to the database."
 }
