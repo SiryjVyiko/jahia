e82a83a845b336bff391dd396b895f61422660ee
SiryjVyiko <dz@hivext.net>
2019-10-24 10:27:13 +0300

    Update setupUser.sh

--- scripts/setupUser.sh
+++ scripts/setupUser.sh
@@ -2,9 +2,9 @@
 
 USER=$1
 PASSWORD=$2
-ADMIN_PASSWORD=`pwgen 10 1`
-JEM=`which jem`
-MYSQL=`which mysql`
+ADMIN_PASSWORD=$(pwgen 10 1)
+JEM=$(which jem)
+MYSQL=$(which mysql)
 cmd="CREATE USER '$USER'@'localhost' IDENTIFIED BY '$PASSWORD'; CREATE USER '$USER'@'%' IDENTIFIED BY '$PASSWORD'; GRANT ALL PRIVILEGES ON *.* TO '$USER'@'localhost' WITH GRANT OPTION; GRANT ALL PRIVILEGES ON *.* TO '$USER'@'%' WITH GRANT OPTION; FLUSH PRIVILEGES;"
 unset resp;
 resp=$(mysql -u$USER -p$PASSWORD mysql --execute="SHOW COLUMNS FROM user")
