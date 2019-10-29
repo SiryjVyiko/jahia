987ac34b8abe7539984cb63eacffd5ee49782d6e
SiryjVyiko <dz@hivext.net>
2019-10-29 11:19:39 +0200

    Create setupUser2.sh

---
+++ scripts/setupUser2.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+USER=$1
+PASSWORD=$2
+ADMIN_PASSWORD=$(pwgen 10 1)
+JEM=$(which jem)
+MYSQL=$(which mysql)
+cmd="CREATE USER '$USER'@'localhost' IDENTIFIED BY '$PASSWORD'; CREATE USER '$USER'@'%' IDENTIFIED BY '$PASSWORD'; GRANT ALL PRIVILEGES ON *.* TO '$USER'@'localhost' WITH GRANT OPTION; GRANT ALL PRIVILEGES ON *.* TO '$USER'@'%' WITH GRANT OPTION; FLUSH PRIVILEGES;"
+unset resp;
+resp=$(mysql -u$USER -p$PASSWORD mysql --execute="SHOW COLUMNS FROM user")
+res=$?
+[ "$res" -eq 0 ] && {
+   echo "[Info] User $user has the required access to the database."
+} || {
+   $JEM passwd set -p $ADMIN_PASSWORD
+   $MYSQL -uroot -p${ADMIN_PASSWORD} --execute="$cmd"
+}
